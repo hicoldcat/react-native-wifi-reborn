2d7c74da4cdeb868059873aabcbd449042f09044
hicoldcat <hicoldcat@foxmail.com>
2024-02-27 01:26:28 +0800

    test for gateway on ios

--- ios/RNWifi.m
+++ ios/RNWifi.m
@@ -6,6 +6,9 @@
 
 #import "ConnectError.h"
 
+#import <arpa/inet.h>
+#import <netinet/in.h>
+#import <ifaddrs.h>
 
 @interface WifiManager () <CLLocationManagerDelegate>
 @property (nonatomic,strong) CLLocationManager *locationManager;
@@ -24,6 +27,56 @@
           }
   }
   return self;
+}
+
+- (NSString *)getGatewayIpForCurrentWiFi {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    NSLog(@"本机地址：%@",address);
+                    
+                    //routerIP----192.168.1.255 广播地址
+                    NSLog(@"广播地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)]);
+                    
+                    //--192.168.1.106 本机地址
+                    NSLog(@"本机地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]);
+                    
+                    //--255.255.255.0 子网掩码地址
+                    NSLog(@"子网掩码地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_netmask)->sin_addr)]);
+                    
+                    //--en0 接口
+                    //  en0       Ethernet II    protocal interface
+                    //  et0       802.3             protocal interface
+                    //  ent0      Hardware device interface
+                    NSLog(@"接口名：%@",[NSString stringWithUTF8String:temp_addr->ifa_name]);
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    
+    freeifaddrs(interfaces);
+
+    in_addr_t i = inet_addr([address cStringUsingEncoding:NSUTF8StringEncoding]);
+    in_addr_t* x = &i;
+
+    unsigned char *s = getdefaultgateway(x);
+    NSString *ip=[NSString stringWithFormat:@"%d.%d.%d.%d",s[0],s[1],s[2],s[3]];
+    
+    free(s);
+    return ip;
 }
 
 - (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
@@ -85,7 +138,7 @@ RCT_EXPORT_METHOD(connectToSSIDPrefix:(NSString*)ssid
      } else {
          reject([ConnectError code:UnavailableForOSVersion], @"Not supported in iOS<13.0", nil);
      }
- }
+}
 
 RCT_EXPORT_METHOD(connectToProtectedSSIDPrefix:(NSString*)ssid
                   withPassphrase:(NSString*)passphrase
@@ -260,6 +313,12 @@ RCT_REMAP_METHOD(getCurrentWifiSSID,
             reject([ConnectError code:CouldNotDetectSSID], @"Cannot detect SSID", nil);
         }];
     }
+}
+
+RCT_EXPORT_METHOD(getGatewayAddress:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    NSString *gatewayIP = [self getGatewayIpForCurrentWiFi];
+    resolve(gatewayIP);
 }
 
 - (NSString *)parseError:(NSError *)error {
